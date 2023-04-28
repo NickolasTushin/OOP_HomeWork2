cb22cf4d2dbe6d5e25c3cdd7d2d243df00a7b035
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2023-04-28 10:06:08 +0300

    UPD Task

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MainClass {
+public class Main {
     public static void main(String[] args) {
         Plate plate = new Plate(100);
         plate.info();
